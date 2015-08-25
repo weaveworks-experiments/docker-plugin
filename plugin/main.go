8e964b9bba5f1704891e0037e137bb83076cc6ac
Telmo <telmox@gmail.com>
2015-08-25 13:11:38 -0400

    Updating location for the listening sock

--- plugin/main.go
+++ plugin/main.go
@@ -4,9 +4,10 @@ import (
 	"flag"
 	"fmt"
 
+	"os"
+
 	"github.com/weaveworks/docker-plugin/plugin/driver"
 	. "github.com/weaveworks/weave/common"
-	"os"
 )
 
 var version = "(unreleased version)"
@@ -21,7 +22,7 @@ func main() {
 
 	flag.BoolVar(&justVersion, "version", false, "print version and exit")
 	flag.BoolVar(&debug, "debug", false, "output debugging info to stderr")
-	flag.StringVar(&address, "socket", "/usr/share/docker/plugins/weave.sock", "socket on which to listen")
+	flag.StringVar(&address, "socket", "/run/docker/plugins/weave.sock", "socket on which to listen")
 	flag.StringVar(&nameserver, "nameserver", "", "nameserver to provide to containers")
 
 	flag.Parse()
